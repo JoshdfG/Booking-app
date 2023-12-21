030d827595ed2838131a04f42b550f66fb3bf015
JoshdfG <agboolajoshua161@gmail.com>
2023-12-21 22:41:39 +0100

    modified:   src/app/bus/page.tsx

--- src/app/bus/page.tsx
+++ src/app/bus/page.tsx
@@ -73,7 +73,7 @@ const Bus: React.FC = () => {
         </div>
 
         <article>
-          <div className="card bg-gray-200/10 text-center p-6 m-4  mx-auto rounded-2xl text-white/80 ">
+          <div className="card backdrop-blur-sm bg-gray-200/10 text-center p-6 m-4  mx-auto rounded-2xl text-white/80 ">
             <div>
               <h1 className="font-semibold capitalize text-2xl mb-16 mt-8">
                 Reserve your Seats Today
