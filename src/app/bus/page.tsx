a8c1b4eae26af6f95e2777618c55553604905abf
JoshdfG <agboolajoshua161@gmail.com>
2023-12-31 00:09:09 +0100

    modified:   src/app/bus/page.tsx

--- src/app/bus/page.tsx
+++ src/app/bus/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import {useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Image from "next/image";
